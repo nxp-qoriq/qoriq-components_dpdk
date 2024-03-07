DPDK/dpdk

test/bonding: add missing check
CI found the function without checking return value in this place.

Coverity issue: 403101
Fixes: 92073ef961ee ("bond: unit tests")
Cc: stable@dpdk.org

Signed-off-by: Long Wu <long.wu@corigine.com>
Reviewed-by: Chaoyong He <chaoyong.he@corigine.com>
Reviewed-by: Peng Zhang <peng.zhang@corigine.com>
Acked-by: Ferruh Yigit <ferruh.yigit@amd.com>

app/test/test_link_bonding.c
@@ -449,7 +449,8 @@ test_add_already_bonding_member_to_bonding_device(void)
 	uint16_t members[RTE_MAX_ETHPORTS];
 	char pmd_name[RTE_ETH_NAME_MAX_LEN];
 
-	test_add_member_to_bonding_device();
+	TEST_ASSERT_SUCCESS(test_add_member_to_bonding_device(),
+			"Failed to add member to bonding device");
 
 	current_member_count = rte_eth_bond_members_get(test_params->bonding_port_id,
 			members, RTE_MAX_ETHPORTS);

nxp-qoriq/qoriq-components_dpdk

[ upstream commit 0ecafc2e44268e4d0626dcf0c811590466291f20 ]

app/test/test_link_bonding.c
@@ -447,7 +447,8 @@ test_add_already_bonded_slave_to_bonded_device(void)
 	uint16_t slaves[RTE_MAX_ETHPORTS];
 	char pmd_name[RTE_ETH_NAME_MAX_LEN];
 
-	test_add_slave_to_bonded_device();
+	TEST_ASSERT_SUCCESS(test_add_slave_to_bonded_device(),
+			"Failed to add member to bonding device");
 
 	current_slave_count = rte_eth_bond_slaves_get(test_params->bonded_port_id,
 			slaves, RTE_MAX_ETHPORTS);